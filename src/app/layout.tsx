4c943a7b3d2fa111ac43f78da3f38f514040bbcc
samridh09 <samrockerislive04@gmail.com>
2026-05-21 18:00:09 +0530

    refactor: remove PermissionProvider from layout components for cleaner structure

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { AuthProvider } from "@/providers/auth-provider";
+import { PermissionProvider } from "@/hooks/usePermissions";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -34,7 +35,11 @@ export default function RootLayout({
         suppressHydrationWarning
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <AuthProvider>{children}</AuthProvider>
+        <AuthProvider>
+          <PermissionProvider>
+            {children}
+          </PermissionProvider>
+        </AuthProvider>
       </body>
     </html>
   );
